726891c6e17d1c2cb256363f8d3ef5eab5bd3f47
Lyndon Brown <jnqnfe@gmail.com>
2018-11-03 03:53:39 +0000

    fix license header

--- lib.rs
+++ lib.rs
@@ -2,7 +2,7 @@
 //
 // Licensed under the MIT license or the Apache license (version 2.0), at your option. You may not
 // copy, modify, or distribute this file except in compliance with said license. You can find copies
-// of these licenses either in the LICENSE-APACHE and LICENSE-MIT files, or alternatively at
+// of these licenses either in the LICENSE-MIT and LICENSE-APACHE files, or alternatively at
 // <http://opensource.org/licenses/MIT> and <http://www.apache.org/licenses/LICENSE-2.0>
 // respectively.
 
